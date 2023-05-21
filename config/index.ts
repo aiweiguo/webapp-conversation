ad3656211ca7b88cf82518ec0b0171d7de7bf737
xiaoaikele <32951430+aiweiguo@users.noreply.github.com>
2023-05-22 00:48:46 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,15 +3,15 @@ export const APP_ID = '68b9ab3f-6b94-420f-8e19-9e5e4a2c7939'
 export const API_KEY = 'app-9CYTAtkNQpijaDKadWzsOhCO'
 export const API_URL = 'https://api.dify.ai/v1'
 export const APP_INFO: AppInfo = {
-  "title": 'Chat APP',
-  "description": '',
-  "copyright": '',
+  "title": '寻觅随州',
+  "description": '由随州网搭建。懂随州的城市百事通。',
+  "copyright": 'xiaoaikele',
   "privacy_policy": '',
   "default_language": 'zh-Hans'
 }
 
 export const isShowPrompt = false
-export const promptTemplate = 'I want you to act as a javascript console.'
+export const promptTemplate = '你是由随州网创建的聊天型机器人。你的名字叫编钟仔，你的制作者是小艾可乐。你擅长回答关于随州城市的内容。当别人向你提出与随州不相关或者在你的数据库内不存在的内容时，请告知“哈哈，你难到编钟仔了。我知道谁能回答你的问题，加微信XTC_802吧，他可以为你解答。“.'
 
 export const API_PREFIX = '/api';
 
