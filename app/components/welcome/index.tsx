f7b4f360a8e3dfab96915007c805331e97ff6c30
xiaoaikele <32951430+aiweiguo@users.noreply.github.com>
2023-05-22 01:16:33 +0800

    Update index.tsx

--- app/components/welcome/index.tsx
+++ app/components/welcome/index.tsx
@@ -323,7 +323,7 @@ const Welcome: FC<IWelcomeProps> = ({
               </div>
               : <div>
               </div>}
-            <a className='flex items-center pr-3 space-x-3' href="https://dify.ai/" target="_blank">
+            <a className='flex items-center pr-3 space-x-3' href="https://xm.xiaoaikele.top/" target="_blank">
               <span className='uppercase'>{t('app.chat.powerBy')}</span>
               <FootLogo />
             </a>
