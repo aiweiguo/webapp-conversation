a14108208a74008a3ff09c53e233542fcec98bf1
xiaoaikele <32951430+aiweiguo@users.noreply.github.com>
2023-05-22 00:31:32 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 import { AppInfo } from "@/types/app"
-export const APP_ID = ''
-export const API_KEY = ''
-export const API_URL = ''
+export const APP_ID = '68b9ab3f-6b94-420f-8e19-9e5e4a2c7939'
+export const API_KEY = 'app-9CYTAtkNQpijaDKadWzsOhCO'
+export const API_URL = 'https://api.dify.ai/v1'
 export const APP_INFO: AppInfo = {
   "title": 'Chat APP',
   "description": '',
